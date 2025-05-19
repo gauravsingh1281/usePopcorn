6f9dcf94ef1ac6332d154875a21b719641dca3b4
Gaurav Pratap Singh <gs492979@gmail.com>
2025-05-19 13:17:47 +0530

    flexible star rating component created.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 import StarRating from "./components/StarRating";
+function Test() {
+  const [movieRated, setMovieRated] = useState(0);
+  return (
+    <div>
+      <StarRating color={"blue"} maxRating={7} onSetRating={setMovieRated} />
+      <p>Movie was rated {movieRated} star</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} size={34} color={"lime"} defaultRating={3} />
+    <Test />
   </StrictMode>
 );
